b4c87da2112394f578a0b39ba9b2347254ce9f7b
leelife96 <leelife77@naver.com>
2024-08-24 13:11:18 +0900

    first commit

---
+++ final-btc-backend/src/main/java/com/spring/btc/FinalBtcBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spring.btc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalBtcBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalBtcBackendApplication.class, args);
+	}
+
+}
