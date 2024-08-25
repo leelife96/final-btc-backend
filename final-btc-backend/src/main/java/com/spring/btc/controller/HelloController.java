bfca264f750746969e13b916ef35e6a5df093852
leelife96 <leelife77@naver.com>
2024-08-26 00:05:30 +0900

    test

--- final-btc-backend/src/main/java/com/spring/btc/controller/HelloController.java
+++ final-btc-backend/src/main/java/com/spring/btc/controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-	@GetMapping("/")
+	@GetMapping("/vue")
 	public String index() {
 		return "index.html";
 	}
--- final-btc-backend/src/main/java/com/spring/btc/controller/TestController.java
+++ final-btc-backend/src/main/java/com/spring/btc/controller/TestController.java
@@ -1,17 +1,27 @@
 package com.spring.btc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Controller
+@RestController
 public class TestController {
 	
-	@GetMapping("/test")
-	public void getTest() {
-		log.info("테스트 콘트롤러");
+	@CrossOrigin(origins = "http://localhost:8080")
+	@GetMapping("/")
+	public String home() {
+		return "Data 준비중...";
 	}
+	
+	
+	@GetMapping("/connectTest")
+	public String Test() {
+		return "스프링 뷰트 & 뷰 테스트...";
+	}
+	
 	
 }
